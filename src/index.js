9a40ee548a5131389b5abb67903d8b8b23158e39
Dennis Padiernos <depadiernos@gmail.com>
2020-05-06 11:21:24 -0500

    make test a POST instead

--- src/index.js
+++ src/index.js
@@ -1,28 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import axios from 'axios'
 
-import logo from './logo.svg';
-import './index.css';
+import logo from './logo.svg'
+import './index.css'
 
 function App() {
-
-  const clickHandler = async (url) => {
-    const res = await axios.get(url, {headers: {"Authorization":"My Pretend Token Here"}})
+  const clickHandler = async url => {
+    const res = await axios.post(url, { message: 'hi world' }, { headers: { Authorization: 'My Pretend Token Here' } })
     console.log(res.body)
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome
-        </p>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>Welcome</p>
       </header>
-      <button onClick={() => clickHandler("https://blissful-roentgen-8da48d.netlify.app/search2")}>Hit Me!</button>
+      <button onClick={() => clickHandler('https://blissful-roentgen-8da48d.netlify.app/search2')}>Hit Me!</button>
     </div>
-  );
+  )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
